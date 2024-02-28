dfa94b1772d946fe3a2b964a690d6ed68ab41a58
alex <sytnikov.zp@gmail.com>
2024-02-28 14:50:13 +0200

    Add country constructor

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
 'use strict';
+
+function countryConstructor(
+  title,
+  capital = 'Kiev',
+  population = '43790000',
+  area = '603628'
+) {
+  this.title = title;
+  this.capital = capital;
+  this.population = population;
+  this.area = area;
+
+  this.showCountry = function () {
+    return `Title: ${this.title}, Capital:  ${this.capital}, Population: ${this.population}, Area: ${this.area}`;
+  };
+}
+
+const Ukraine = new countryConstructor('Ukraine');
+const USA = new countryConstructor('USA', 'Washington', 331900000, 9864000);
+
+console.log(Ukraine.showCountry());
+console.log('==========================');
+console.log(USA);
+
+function allProperties(country) {
+  for (let key in country) {
+    if (typeof country[key] !== 'function') {
+      console.log(`${key} : ${country[key]}`);
+    }
+  }
+}
+
+console.log('==========================');
+allProperties(Ukraine);
+
+console.log('==========================');
+allProperties(USA);
